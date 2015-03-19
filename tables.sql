93f4b44df2411c05ffb2e0438c74db4f1ab1b85a
ChanderG <chandergovind@gmail.com>
2015-03-19 15:30:47 +0530

    adds city and other geo tables

--- tables.sql
+++ tables.sql
@@ -1,8 +1,26 @@
 DROP TABLE Spot;
 DROP TABLE City;
+DROP TABLE State;
+DROP TABLE Country;
+
+CREATE TABLE Country (
+  CountryID INT PRIMARY KEY,
+  CountryName VARCHAR(30)
+);
+
+CREATE TABLE State (
+  StateID INT PRIMARY KEY,
+  StateName VARCHAR(30),
+  CountryID INT,
+  CONSTRAINT state_c_fk FOREIGN KEY (CountryID) REFERENCES Country (CountryID)
+);
 
 CREATE TABLE City (
-  CityID INT PRIMARY KEY
+  CityID INT PRIMARY KEY,
+  CityName VARCHAR(30),
+  CityPin CHAR(6),
+  StateID INT,
+  CONSTRAINT city_s_fk FOREIGN KEY (StateID) REFERENCES State (StateID)
 );
 
 CREATE TABLE Spot (
