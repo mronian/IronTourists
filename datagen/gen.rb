76bdd7ad4f5c0b21ee7328ab8461654954ebf8ab
ChanderG <chandergovind@gmail.com>
2015-03-24 19:50:24 +0530

    adds city, state, country and some indian defaults

--- datagen/gen.rb
+++ datagen/gen.rb
@@ -1,26 +1,40 @@
 require 'rubygems'
 require 'faker'
 
+Faker::Config.locale = 'en-IND'
+
+#country
 i = 1
-num = 1;
-while i <= num do
-  i_country = "INSERT INTO TABLE Country values ( #{i}, '" + Faker::Address.country + "');"
-  puts (i_country)
+numCtry = 1;
+while i <= numCtry do
+  i_item = "INSERT INTO TABLE Country values ( #{i}, '" + Faker::Address.country + "');"
+  puts (i_item)
   i += 1
 end  
 
 puts ()
 puts ()
 
+#state
 i = 1
-num = 25;
-while i <= num do
-  i_country = "INSERT INTO TABLE State values ( #{i}, '" + Faker::Address.state + "', 1);"
-  puts (i_country)
+numState = 25;
+while i <= numState do
+  i_item = "INSERT INTO TABLE State values ( #{i}, '" + Faker::Address.state + "', 1);"
+  puts (i_item)
   i += 1
 end  
 
 puts ()
 puts ()
 
+#city
+i = 1
+numCity = 100;
+while i <= numCity do
+  i_item = "INSERT INTO TABLE City values ( #{i}, '" + Faker::Address.city + "', '" + Faker::Address.zip + "', #{1 + rand(numState)} );"
+  puts (i_item)
+  i += 1
+end  
 
+puts ()
+puts ()
