dbfddbddefda1fb2394e9b5f91ec01063a4e9e12
ChanderG <chandergovind@gmail.com>
2015-03-24 20:12:12 +0530

    checks script;corrects errors

--- datagen/gen.rb
+++ datagen/gen.rb
@@ -7,7 +7,7 @@ Faker::Config.locale = 'en-IND'
 i = 1
 numCtry = 1;
 while i <= numCtry do
-  i_item = "INSERT INTO TABLE Country values ( #{i}, '" + Faker::Address.country + "');"
+  i_item = "INSERT INTO Country values ( #{i}, '" + Faker::Address.country + "');"
   puts (i_item)
   i += 1
 end  
@@ -19,7 +19,7 @@ puts ()
 i = 1
 numState = 25;
 while i <= numState do
-  i_item = "INSERT INTO TABLE State values ( #{i}, '" + Faker::Address.state + "', 1);"
+  i_item = "INSERT INTO State values ( #{i}, '" + Faker::Address.state + "', 1);"
   puts (i_item)
   i += 1
 end  
@@ -29,9 +29,21 @@ puts ()
 
 #city
 i = 1
-numCity = 100;
+numCity = 75;
 while i <= numCity do
-  i_item = "INSERT INTO TABLE City values ( #{i}, '" + Faker::Address.city + "', '" + Faker::Address.zip + "', #{1 + rand(numState)} );"
+  i_item = "INSERT INTO City values ( #{i}, '" + Faker::Address.city.tr('\'', '') + "', '" + Faker::Address.zip.rjust(6,'0') + "', #{1 + rand(numState)} );"
+  puts (i_item)
+  i += 1
+end  
+
+puts ()
+puts ()
+
+#spot
+i = 1
+numSpot = 125;
+while i <= numSpot do
+  i_item = "INSERT INTO Spot values ( #{i}, '" + Faker::Address.city.tr('\'', '') + "', '" + Faker::Address.street_address.tr('\'', '') + "', #{1 + rand(numCity)} );"
   puts (i_item)
   i += 1
 end  
--- datagen/issues.md
+++ datagen/issues.md
@@ -1,1 +1,2 @@
 1. States and pincodes do not correlate.
+2. Spots are being generated using City itself. Override with some values like Temple, Forts etc.
